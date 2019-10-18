6dc511b6b65e5c9e038f7ad90d57d6dcb0462591
WooJin Song <yeqfx@hotmail.com>
2019-10-19 01:47:32 +0900

    Banner Script

--- standard/js/script.js
+++ standard/js/script.js
@@ -1,3 +1,13 @@
+// 배너
+$('.ban').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true
+});
+
 // 버튼을 클릭하면 전체메뉴를 보이게 하세요
 $(".tit .btn").click(function(e) {
     e.preventDefault();
